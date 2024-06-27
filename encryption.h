bf77dd0775582132b9698b8778c0e512eb93f3f4
altrek48 <144661671+altrek48@users.noreply.github.com>
2024-06-27 19:24:53 +0300

    Update encryption.h(основные функции для кодировки и декодировки)
    Нужно будет подогнать к классу юзера

--- encryption.h
+++ encryption.h
@@ -1,1 +1,28 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Шифровка
+string encrypt(string input) {
+    const int number = input.size();
+    string result = "";
+    for (int i = 0; i < number; i++) {
+        int symbol = input[i];
+        symbol += 7;
+        result += symbol;
+    }
+    return result;
+}
+
+//Дешифровка
+string decrypt(string input) {
+    const int number = input.size();
+    string result = "";
+    for (int i = 0; i < number; i++) {
+        int symbol = input[i];
+        symbol -= 7;
+        result += symbol;
+    }
+    return result;
+}
