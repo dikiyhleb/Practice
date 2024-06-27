87dbd3b0ca65a3d3f42de087dd7f871a647754cb
MaximMashnev <maximmashnev345@gmail.com>
2024-06-27 19:44:08 +0300

    Реализована генерация пароля

--- generationPassword.h
+++ generationPassword.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef GENERATIONPASSWORD_H
+#define GENERATIONPASSWORD_H
+
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+class generationPassword
+{
+private:
+    int length;
+    std::string charset;
+
+public:
+    generationPassword();
+    std::string generate();
+};
+
+#endif // GENERATIONPASSWORD_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 ﻿#include <iostream>
 #include <string>
 #include "generationNickname.h";
+#include "generationPassword.h"
 
 using namespace std;
 
 int main()
 {
     cout << genNickname("Bobryatina");
+
+    //Генерация пароля
+    generationPassword genPass;
+    std::string password = genPass.generate();
+    std::cout << "Generated password: " << password << std::endl;
 }
