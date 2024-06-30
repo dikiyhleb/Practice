4f62aa043f0faea41ebfe1d887045efa100725ad
Korovin Sergey <korwork1991@yandex.ru>
2024-06-30 19:47:01 +0300

    Удалён один из вариантов генераций никнейма

--- generationNickname.h
+++ generationNickname.h
@@ -9,58 +9,6 @@
 
 using namespace std;
 
-string genNicknameByLeet(string defaultNick) {
-	for (int i = 1; i < defaultNick.length(); i++) {
-		switch (defaultNick[i])
-		{
-		case 'I':
-		case 'i':
-			defaultNick[i] = '1';
-			break;
-		case 'A':
-		case 'a':
-			defaultNick[i] = '4';
-			break;
-		case 'G':
-		case 'g':
-			defaultNick[i] = '6';
-			break;
-		case 'B':
-		case 'b':
-			defaultNick[i] = '8';
-			break;
-		case 'O':
-		case 'o':
-			defaultNick[i] = '0';
-			break;
-		case 'E':
-		case 'e':
-			defaultNick[i] = '3';
-			break;
-		case 'L':
-		case 'l':
-			defaultNick[i] = '1';
-			break;
-		case 'S':
-		case 's':
-			defaultNick[i] = '5';
-			break;
-		case 'T':
-		case 't':
-			defaultNick[i] = '7';
-			break;
-		case 'Z':
-		case 'z':
-			defaultNick[i] = '2';
-			break;
-		default:
-			break;
-		}
-		i++;
-	}
-	return defaultNick;
-}
-
 string genNicknameByBase() {
 	ifstream file("nicknameBase.txt");
 	string line;
