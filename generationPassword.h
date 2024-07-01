f9f43db3f1703a839c49eb0972ce95083c1959d2
MaximMashnev <maximmashnev345@gmail.com>
2024-07-01 13:10:17 +0300

    Супер обнова генерации пароля

--- generationPassword.h
+++ generationPassword.h
@@ -12,8 +12,22 @@ private:
     std::string charset;
 
 public:
-    generationPassword();
-    std::string generate();
+    generationPassword()
+        : charset("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    {
+        srand(time(0));
+        length = rand() % 5 + 8; // случайная длина пароля от 8 до 12 символов
+    }
+
+    std::string generate()
+    {
+        std::string password;
+        for (int i = 0; i < length; ++i)
+        {
+            password += charset[rand() % charset.length()];
+        }
+        return password;
+    }
 };
 
 #endif // GENERATIONPASSWORD_H
